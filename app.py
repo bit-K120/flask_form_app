5020ed3385e893ba4287877c29d1bd936f39dcfe
bit-K120 <kayugen105@gmail.com>
2023-10-07 14:21:34 +0900

    codes refactored

--- app.py
+++ app.py
@@ -3,13 +3,14 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
-@app.route('/', methods = ["GET", "POST"])
+@app.route('/', methods = ["GET"])
+def index_get():
+    return render_template('index.html')
+
+@app.route('/', methods = ["POST"])
 def index():
-    if request.method == "GET":
-        return render_template('index.html')
-    else: 
-        message = request.form["message"]
-        return render_template("index.html", message = message)
+    message = request.form["message"]
+    return render_template("index.html", message = message)
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8000, debug=True)
